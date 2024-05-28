dffd082b20e59512220b04afbd5db792114b5525
AlwaysOMG <alan135790.iem06@nctu.edu.tw>
2024-05-29 00:47:59 +0800

    implement alns (2024)

--- model_test.py
+++ model_test.py
@@ -1,9 +1,13 @@
+import time
+
 from dvrp.dvrp import DVRP
 from dvrp.route_manager import RouteManager
 from model.new_dynamic_attention_model.dynamic_attention_model import DynamicAttentionModel
+from agent.alns_agent import ALNS_Solver
 
 import configparser
 config = configparser.ConfigParser()
+config.read("./config.cfg")
 customer_num = int(config["instance"]["customer_num"])
 
 env = DVRP()
@@ -12,10 +16,10 @@ model = DynamicAttentionModel(customer_num, mgr.get_feature_dim())
 
 obs = env.reset()
 while True:
-    obs_tensor, obs_info = mgr.obs_to_tensor(obs)
-    action, prob = model(obs_tensor, obs_info, True)
-    route = mgr.action_to_route(action)
-    obs, reward, done = env.step(route)
+    sol = ALNS_Solver(obs).run()
+    route = mgr.action_to_route(sol)
+    obs, reward, is_done = env.step(route)
 
-    if done:
+    if is_done:
+        print(reward)
         break
