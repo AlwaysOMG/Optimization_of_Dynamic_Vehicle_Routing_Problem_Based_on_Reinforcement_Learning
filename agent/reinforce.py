fix the problem of  gradient
add layer norm in encoder, add residual conn in decoder, clip the grad, read the customer num directly

agent/reinforce.py
@@ -44,12 +44,19 @@ class REINFORCE:
 
         self.episode_trajectory = []
 
-    def update_parameter(self):
+    def update_parameter(self, show_grad=False):
         loss = self.batch_loss / self.batch_size
         self.optimizer.zero_grad()
         loss.backward()
+        torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=10.0)
         self.optimizer.step()
 
+        if show_grad:
+            for name, param in self.model.named_parameters():
+                if param.grad is not None:
+                    grad_norm = param.grad.norm().item()
+                    print(f'Gradient norm for {name}: {grad_norm}')
+        
         self.batch_loss = 0
 
     def save_model(self, epoch):

model/new_dynamic_attention_model/decoder.py
@@ -4,8 +4,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions import Categorical
 
-torch.manual_seed(0)
-
 class ProbLayer(nn.Module):
     def __init__(self, embed_dim, clip_c):
         super().__init__()
@@ -16,7 +14,7 @@ class ProbLayer(nn.Module):
     def forward(self, x, mask):
         q = self.q_weight(x[0])
         k = self.k_weight(x[1])
-        u = self.clip_c * torch.tanh(torch.matmul(q, k.T))
+        u = self.clip_c * F.tanh(torch.matmul(q, k.T))
         mask_u = torch.where(mask, float('-inf'), u)
         prob = F.softmax(mask_u, dim=-1)
 
@@ -25,10 +23,13 @@ class ProbLayer(nn.Module):
 class Decoder(nn.Module):
     vehicle_info = None
     node_info = None
+    travel_time_matrix = None
+    decision_time = None
+    current_time = None
 
     def __init__(self, embed_dim, num_heads, clip_c, device):
         super().__init__()
-        self.combined_layer = nn.Linear(embed_dim*2+1, embed_dim)
+        self.combined_layer = nn.Linear(embed_dim*2+2, embed_dim)
         self.attn_layer = nn.MultiheadAttention(embed_dim, num_heads, batch_first=True)
         self.prob_layer = ProbLayer(embed_dim, clip_c)
         self.device = device
@@ -44,16 +45,29 @@ class Decoder(nn.Module):
             if target_node_id != 0:
                 self.node_info[target_node_id][1] = True
 
+    def set_travel_time(self, travel_time):
+        self.travel_time_matrix = travel_time
+
+    def set_decision_time(self, decision_time):
+        self.decision_time = decision_time
+        self.current_time = decision_time
+
     def make_mean_mask(self):
-        # mask the served customer       
+        # mask the served customer
         return torch.tensor([node[1] == True for node in self.node_info])
 
     def make_vehicle_embed(self, x, vehicle_id):
         mask = self.make_mean_mask()
-        mean_embed = x[~mask].mean(dim=0).unsqueeze(0)
-        selected_node_embed = x[self.vehicle_info[vehicle_id][0]].unsqueeze(0)
-        capacity_tensor = torch.tensor([[self.vehicle_info[vehicle_id][1]]], dtype=torch.float32).to(self.device)
-        combined_embed = self.combined_layer(torch.cat((mean_embed, selected_node_embed, capacity_tensor), dim=1))
+        safe_capacity = max(self.vehicle_info[vehicle_id][1], 1e-6)
+        safe_total_capacity = max(sum(info[1] for info in self.vehicle_info), 1e-6)
+        safe_current_time = max(self.current_time, 1e-6)
+        
+        with torch.no_grad():
+            mean_embed = x[~mask].mean(dim=0).unsqueeze(0)
+            selected_node_embed = x[self.vehicle_info[vehicle_id][0]].unsqueeze(0)
+            capacity_tensor = torch.tensor([[safe_capacity/safe_total_capacity]], dtype=torch.float32).to(self.device)
+            time_tensor = torch.tensor([[safe_current_time/1000]], dtype=torch.float32).to(self.device)
+        combined_embed = self.combined_layer(torch.cat((mean_embed, selected_node_embed, capacity_tensor, time_tensor), dim=1))
 
         return combined_embed
     
@@ -79,6 +93,11 @@ class Decoder(nn.Module):
         return id
     
     def update_info(self, vehicle_id, node_id):
+        if node_id != 0:
+            self.current_time += self.travel_time_matrix[self.vehicle_info[vehicle_id][0], node_id].item()
+        else:
+            self.current_time = copy.deepcopy(self.decision_time)
+        
         self.vehicle_info[vehicle_id][0] = node_id
         self.vehicle_info[vehicle_id][1] -= self.node_info[node_id][0]
 
@@ -94,8 +113,9 @@ class Decoder(nn.Module):
             while True:
                 vehicle_embed = self.make_vehicle_embed(x, vehicle_id)
                 attn_mask = self.make_attn_mask(vehicle_id)
-                vehicle_embed = self.attn_layer(vehicle_embed, x, x, 
+                context_vector = self.attn_layer(vehicle_embed, x, x, 
                                                 key_padding_mask=attn_mask)[0]
+                vehicle_embed = torch.add(vehicle_embed, context_vector)
                 
                 prob = self.prob_layer([vehicle_embed, x], attn_mask)
                 node_id = self.sample_customer(prob, is_greedy)              

model/new_dynamic_attention_model/dynamic_attention_model.py
@@ -5,20 +5,20 @@ import torch.nn as nn
 from model.new_dynamic_attention_model.encoder import Encoder
 from model.new_dynamic_attention_model.decoder import Decoder
 
-config = configparser.ConfigParser()
-config.read("./config.cfg")
-parameter_config = config['parameter']
-
 class DynamicAttentionModel(nn.Module):
     device = torch.device("cuda:0")
+
+    config = configparser.ConfigParser()
+    config.read("./config.cfg")
+    parameter_config = config['parameter']
     embed_dim = int(parameter_config["embed_dim"])
     num_heads = int(parameter_config["num_heads"])
     num_layers = int(parameter_config["num_layers"])
     clip_c = int(parameter_config["clip_c"])
+    customer_num = int(config["instance"]["customer_num"])
 
-    def __init__(self, customer_num, feature_dim):
+    def __init__(self, feature_dim):
         super().__init__()
-        self.customer_num = customer_num
         static_feature_dim = feature_dim[0]
         dynamic_feature_dim = feature_dim[1]
         self.encoder = Encoder(static_feature_dim, dynamic_feature_dim, 
@@ -32,6 +32,8 @@ class DynamicAttentionModel(nn.Module):
     def forward(self, x, info, is_greedy):
         self.decoder.set_vehicle_info(info[0])
         self.decoder.set_node_info(info[1])
+        self.decoder.set_decision_time(info[2])
+        self.decoder.set_travel_time(x[1])
 
         encoder_output = self.encoder(x)
         route, prob = self.decoder(encoder_output, is_greedy)

model/new_dynamic_attention_model/encoder.py
@@ -2,9 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-torch.manual_seed(0)
-
-class Sublayer(nn.Module):
+class SubLayer(nn.Module):
     def __init__(self, embed_dim, num_heads, ff_dim):
         super().__init__()
         self.attn_layer = nn.MultiheadAttention(embed_dim, num_heads, batch_first=True)
@@ -12,13 +10,13 @@ class Sublayer(nn.Module):
         self.norm_2 = nn.LayerNorm(embed_dim)
         self.ff_1 = nn.Linear(embed_dim, ff_dim)
         self.ff_2 = nn.Linear(ff_dim, embed_dim)
-    
+
     def forward(self, x):
         attn_output = self.attn_layer(x, x, x)[0]
-        tanh_1_output = torch.tanh(self.norm_1(torch.add(x, attn_output)))
+        tanh_1_output = F.tanh(self.norm_1(torch.add(x, attn_output)))
         ff_1_output = self.ff_1(tanh_1_output)
         ff_2_output = self.ff_2(F.relu(ff_1_output))
-        tanh_2_output = torch.tanh(self.norm_2(torch.add(tanh_1_output, ff_2_output)))
+        tanh_2_output = F.tanh(self.norm_2(torch.add(tanh_1_output, ff_2_output)))
 
         return tanh_2_output
 
@@ -31,15 +29,23 @@ class Encoder(nn.Module):
         self.num_layers = num_layers
 
         self.static_embed = nn.Linear(static_feature_dim, embed_dim)
-        self.static_layers = nn.ModuleList([Sublayer(embed_dim, num_heads, self.ff_dim) for _ in range(num_layers)])
+        self.static_layers = nn.ModuleList([SubLayer(embed_dim, num_heads, self.ff_dim) for _ in range(num_layers)])
         
         self.dynamic_embed = nn.Linear(dynamic_feature_dim, embed_dim)
-        self.dynamic_layers = nn.ModuleList([Sublayer(embed_dim, num_heads, self.ff_dim) for _ in range(num_layers)])
+        self.dynamic_layers = nn.ModuleList([SubLayer(embed_dim, num_heads, self.ff_dim) for _ in range(num_layers)])
 
+        self.linear_layer = nn.Linear(embed_dim*2, embed_dim)
         self.fusion_layer = nn.MultiheadAttention(embed_dim, num_heads, batch_first=True)
     
+    def feature_normalize(self, x):
+        mins = torch.min(x, dim=0).values
+        maxs = torch.max(x, dim=0).values
+        norm_x = (x - mins) / (maxs - mins)
+        return norm_x
+
     def forward(self, x):        
         static_x = x[0].to(self.device)
+        static_x = self.feature_normalize(static_x)
         static_x = self.static_embed(static_x)
         for i in range(self.num_layers):
             static_x = self.static_layers[i](static_x)
@@ -49,7 +55,7 @@ class Encoder(nn.Module):
         for i in range(self.num_layers):
             dynamic_x = self.dynamic_layers[i](dynamic_x)
 
-        #combined_x = self.combined_layer(torch.cat((static_x, dynamic_x), dim=1))
-        combined_x = self.fusion_layer(dynamic_x, static_x, static_x)[0]
+        combined_x = self.linear_layer(torch.cat((static_x, dynamic_x), dim=1))
+        fushion_x = self.fusion_layer(combined_x, combined_x, combined_x)[0]
         
-        return combined_x
+        return fushion_x

test.py
@@ -22,16 +22,16 @@ customer_num = int(config["instance"]["customer_num"])
 # init instance
 env = DVRP()
 mgr = RouteManager(env)
-model = DynamicAttentionModel(customer_num, mgr.get_feature_dim())
-model.load_state_dict(torch.load('./model/new_dynamic_attention_model/parameter/50/128_3_4_46.pth'))
+model = DynamicAttentionModel(mgr.get_feature_dim())
+model.load_state_dict(torch.load('./model/new_dynamic_attention_model/parameter/50/origin.pth'))
 agent = REINFORCE(model, 0, 0)
-writer = Writer(customer_num, is_test=True)
+writer = Writer(is_test=True)
 
 # testing
 for i in trange(test_instance):
     start_time = time.time()
     obs = env.reset()
-    while True:        
+    while True:
         obs_tensor, obs_info = mgr.obs_to_tensor(obs)
         action = agent.get_action(obs_tensor, obs_info, True)
         route = mgr.action_to_route(action)

train.py
@@ -16,14 +16,14 @@ lr = float(train_config["learning_rate"])
 batch_size = int(train_config["batch_size"])
 epochs_num = int(train_config["epochs_num"])
 steps_num = int(train_config["steps_num"])
-customer_num = int(config["instance"]["customer_num"])
 
 # init instance
 env = DVRP()
 mgr = RouteManager(env)
-model = DynamicAttentionModel(customer_num, mgr.get_feature_dim())
+model = DynamicAttentionModel(mgr.get_feature_dim())
+#model.load_state_dict(torch.load('./model/new_dynamic_attention_model/parameter/50/128_3_4_3.pth'))
 agent = REINFORCE(model, lr, batch_size)
-writer = Writer(customer_num)
+writer = Writer()
 
 # training
 for epoch in range(epochs_num):

utils/writer.py
@@ -1,14 +1,19 @@
+import configparser
 import datetime
 from torch.utils.tensorboard import SummaryWriter
 import csv
 
 class Writer:
+    config = configparser.ConfigParser()
+    config.read("./config.cfg")
+    customer_num = int(config["instance"]["customer_num"])
+    
     date = datetime.date.today()
     time = datetime.datetime.now().strftime("%H-%M-%S")
     
-    def __init__(self, customer_num, is_test=False):
+    def __init__(self, is_test=False):        
         if not is_test:
-            self.writer = SummaryWriter(log_dir=f"log/{customer_num}/{self.date}_{self.time}")
+            self.writer = SummaryWriter(log_dir=f"log/{self.customer_num}/{self.date}_{self.time}")
         
         self.episode = 0
         self.step = 0